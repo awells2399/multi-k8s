70c6c057582fbc7f019d52a47d02d390f4e1e829
Aaron Wells <56880210+awells2399@users.noreply.github.com>
2025-06-05 13:32:13 -0400

    inital commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      <h1>Other Page</h1>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
+};
